ab7cd0832252250035b93c4a8e0b23d1cf3ee52b
marcinpietrosian <marcin@pietrosian.pl>
2018-04-20 20:55:49 +0200

    unfinished api test config

--- backend/src/test/groovy/mpietrosian/backend/implementation/ApiV0BeanTest.groovy
+++ backend/src/test/groovy/mpietrosian/backend/implementation/ApiV0BeanTest.groovy
@@ -1,11 +1,24 @@
 import mpietrosian.backend.Application
+import groovyx.net.http.RESTClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.context.embedded.LocalServerPort
 import spock.lang.*
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
 @SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 class ApiV0BeanTest extends Specification {
+
+    @LocalServerPort
+    int serverPort
+
+    String serverHost = "localhost"
+
+    RESTClient restClient = new RESTClient(serverHost + ":" + serverPort, JSON)
+
+    def "return current weather" () {
+        def response = restClient.get()
+    }
 
 
 }
