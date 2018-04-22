50a87c79070c71f891b7e3ca6f1c0f70079e1f08
marcinpietrosian <marcin@pietrosian.pl>
2018-04-22 11:55:27 +0200

    additional api methods

--- backend/src/main/java/mpietrosian/backend/definition/ApiV0.java
+++ backend/src/main/java/mpietrosian/backend/definition/ApiV0.java
@@ -12,24 +12,63 @@ import mpietrosian.backend.implementation.exception.ApiException;
  * Created by mpietrosian on 19.04.2018.
  */
 
-@RequestMapping("/api/v1/data")
+@RequestMapping("/api/v1")
 public abstract class ApiV0 {
+
+    @RequestMapping(
+        value="status/arduino",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getArduinoStatus() throws ApiException;
+
+    @RequestMapping(
+        value="status/server",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getServerStatus() throws ApiException;
+
+    @RequestMapping(
+        value="data/arduino",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getArduinoRawData() throws ApiException;
     
     @RequestMapping(
-        value="weather/current",
+        value="data/weather/current",
         produces="application/json",
         method=RequestMethod.GET)
     public abstract ResponseEntity getCurrentWeather() throws ApiException;
 
     @RequestMapping(
-        value="weather",
+        value="data/weather",
         produces="application/json",
         method=RequestMethod.GET)
     public abstract ResponseEntity getWeather(@RequestParam(value="query", required=true) long from, @RequestParam(value="query", required=true) long to) throws ApiException;
 
     @RequestMapping(
-        value="weather/{label}",
+        value="data/weather/{label}",
         produces="application/json",
         method=RequestMethod.GET)
     public abstract ResponseEntity getWeatherByLabel(@PathVariable String label, @RequestParam(value="query", required=true) long from, @RequestParam(value="query", required=true) long to) throws ApiException;
+
+    @RequestMapping(
+        value="data/fitbit",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getFitbitData(@RequestParam(value="query", required=true) long from, @RequestParam(value="query", required=true) long to) throws ApiException;
+
+    @RequestMapping(
+        value="data/fitbit/{label}",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getFitbitDataByLabel(@PathVariable String label, @RequestParam(value="query", required=true) long from, @RequestParam(value="query", required=true) long to) throws ApiException;
+
+    @RequestMapping(
+        value="data/fitbit/today",
+        produces="application/json",
+        method=RequestMethod.GET)
+    public abstract ResponseEntity getFitbitTodayData() throws ApiException;
+
+    
+
 }
--- backend/src/main/java/mpietrosian/backend/implementation/ApiV0Bean.java
+++ backend/src/main/java/mpietrosian/backend/implementation/ApiV0Bean.java
@@ -24,4 +24,35 @@ public class ApiV0Bean extends ApiV0{
 	public ResponseEntity getWeatherByLabel(String label, long from, long to) throws ApiException {
 		return null;
 	}
+
+	@Override
+	public ResponseEntity getFitbitData(long from, long to) throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getFitbitDataByLabel(String label, long from, long to) throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getFitbitTodayData() throws ApiException {
+		return null;
+    }
+
+	@Override
+	public ResponseEntity getArduinoStatus() throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getServerStatus() throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getArduinoRawData() throws ApiException {
+		return null;
+	}
+    
 }
