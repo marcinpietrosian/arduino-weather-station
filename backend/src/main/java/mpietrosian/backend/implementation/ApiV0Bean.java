86a1839fea1b100a5b89d87394eca2cad97ea646
marcinpietrosian <marcin@pietrosian.pl>
2018-04-22 11:03:44 +0200

    fix api implementation

--- backend/src/main/java/mpietrosian/backend/implementation/ApiV0Bean.java
+++ backend/src/main/java/mpietrosian/backend/implementation/ApiV0Bean.java
@@ -1,9 +1,27 @@
 package mpietrosian.backend.implementation;
 
+import org.springframework.http.ResponseEntity;
+
 import mpietrosian.backend.definition.ApiV0;
+import mpietrosian.backend.implementation.exception.ApiException;
 
 /**
  * Created by mpietrosian on 19.04.2018.
  */
 public class ApiV0Bean extends ApiV0{
+
+	@Override
+	public ResponseEntity getCurrentWeather() throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getWeather(long from, long to) throws ApiException {
+		return null;
+	}
+
+	@Override
+	public ResponseEntity getWeatherByLabel(String label, long from, long to) throws ApiException {
+		return null;
+	}
 }
