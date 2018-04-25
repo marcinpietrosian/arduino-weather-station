ae90d0639b119ed52147768e1ab5777ea5e01a48
marcinpietrosian <marcin@pietrosian.pl>
2018-04-25 09:00:21 +0200

    fix ArduinoServiceImpl

---
+++ backend/src/main/java/mpietrosian/backend/implementation/services/arduino/impl/ArduinoServiceImpl.java
@@ -0,0 +1,7 @@
+package mpietrosian.backend.implementation.services.arduino.impl;
+
+import mpietrosian.backend.implementation.services.arduino.ArduinoService;
+
+public class ArduinoServiceImpl implements ArduinoService {
+    
+}
